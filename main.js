bef5bcbff00bdc3cd5b6feb19a7ea2e37eed0bb2
chanc245 <112438908+chanc245@users.noreply.github.com>
2023-04-06 15:17:22 -0400

    Add file

---
+++ main.js
@@ -0,0 +1,33 @@
+let btn = document.querySelector('button');
+<button>My button</button>
+
+btn.addEventListener("click", () => {
+    let x = Math.floor(Math.random() * 255);
+    let y = Math.floor(Math.random() * 255);
+    let z = Math.floor(Math.random() * 255);
+    let bgColor = "rgb" + x + "," + y + "," + z + ")";
+
+    document.body.style.background = bgColor;
+
+    let a = document.getElementById("form1");
+
+    let b = document.getElementsByTagName("H2");
+    console.log(a);
+    console.log(b);
+
+    let elem = [];
+    let i;
+    for (i = 0; i < b.length; i++) {
+        elem.push(document.createElement("H3"));
+        elem[i].innerHTML = a.elements[i].value;
+        document.body.appendChild(elem[i]);
+    }
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+
+});
+
+removeText.onclick = function () {
+    removeText.remove();
+}
