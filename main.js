3e458bb47a46040799d70fb1f9a59ede29a11bdb
chanc245 <112438908+chanc245@users.noreply.github.com>
2023-04-20 06:29:22 -0400

    page4 DONE

--- main.js
+++ main.js
@@ -8,3 +8,23 @@ document.addEventListener("mousemove", function (event) {
     circle.textContent = "x: " + (x - 20) + ", y: " + (y - 20);
 });
 
+function parseQuote(data) {
+    const quoteText = data.quoteText;
+    document.getElementById('fortune').textContent = quoteText;
+}
+
+const fortune_url = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote'
+async function getFortune() {
+    const fortune_url = 'https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote';
+    const script = document.createElement('script');
+    script.src = fortune_url;
+    document.body.appendChild(script);
+}
+
+getFortune();
+
+// this code is from: https://www.youtube.com/watch?v=uxf0--uiX0I 
+
+// the original example is from: https://codepen.io/ohmycodeness/pen/KzdLRb 
+
+// and fixed by chat GPT: The issue could be that the Forismatic API returns JSONP, which is a different format than regular JSON. JSONP is a JSON response wrapped in a function call, and it's commonly used for cross-domain requests.
