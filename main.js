19098f7cb3c7abea85237deb4664ae6f0358a6ef
chanc245 <112438908+chanc245@users.noreply.github.com>
2023-04-20 02:53:54 -0400

    adding circle that follow the cursor

---
+++ main.js
@@ -0,0 +1,9 @@
+var circle = document.getElementById("circle");
+
+document.addEventListener("mousemove", function (event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    circle.style.left = x + "px";
+    circle.style.top = y + "px";
+    // circle.textContent = "x: " + (x - 20) + ", y: " + (y - 20);
+});
